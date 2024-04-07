9d47edbbf149f34bd0f46e3cae36c64cf7866d6f
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-04-07 12:16:40 +0530

    updated

--- Array/20-missingNumber.cpp
+++ Array/20-missingNumber.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // leetcode - 268
-
+/*
 int missingNumber(vector<int> &arr)
 {
     int n = arr.size();
@@ -15,4 +15,29 @@ int missingNumber(vector<int> &arr)
     }
     int missingElement = totalSum - sum;
     return missingElement;
+}
+*/
+
+// using xor  all index xor ^ all element xor (b ^ b = 0 )(0 ^ b = b) = missing element
+
+int xorr(vector<int> &v)
+{
+    int ans = 0;
+    // xor all values of array
+    for (int i = 0; i < v.size(); i++)
+    {
+        ans = ans ^ v[i];
+    }
+    // xor all range items
+    int n = v.size();
+    for (int i = 0; i <= n; i++)
+    {
+        ans = ans ^ i;
+    }
+    return ans;
+}
+
+int missElement(vector<int> &nums)
+{
+    return xorr(nums);
 }
