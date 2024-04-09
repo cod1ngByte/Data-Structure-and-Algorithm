5a9d2e537871af3322f4942671a0ba9dfc5a3f52
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-04-09 19:10:05 +0530

    updated num

--- Array/test.cpp
+++ Array/test.cpp
@@ -1,20 +1,4 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-
-void fun(int *arr)
-{
-    arr[1] = 200;
-    cout << arr[1];
-}
-
-int main()
-{
-
-    int arr[] = {10, 20, 30, 40};
-
-    cout << arr[1];
-
-    fun(arr);
-    cout << arr[1];
-}
